ae0f88ffb1e502bae8a47a0df6751fc288bdaab3
5e-Rennes <jem.richard35@gmail.com>
2021-07-11 16:29:32 +0200

    Update menu to make it more responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function toggleMenu() {
     var menuBox = document.getElementById('menu-box');
     var icon = document.getElementById("icon")    
-    if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+    if(menuBox.style.display !== "none") { // if is menuBox displayed, hide it
       menuBox.style.display = "none";
     }
     else { // if is menuBox hidden, display it
-      menuBox.style.display = "block";
+      menuBox.style.display = "inline-grid";
+      menuBox.style.gridAutoFlow = "row"
     }
 }
